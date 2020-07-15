08fca56eb18977e0fd725a5a5b1d0568e500e9e1
Nold, Elizabeth <NOLDE@nationwide.com>
2020-07-15 11:35:17 -0400

    Adding some swift stuff

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,51 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    
+    var navigationController: UINavigationController!
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+        ) -> Bool {
+        
+        
+        let controller = self.window.rootViewController as! FlutterViewController
+        
+        linkNativeCode(controller: controller)
+        
+        GeneratedPluginRegistrant.register(with: self)
+        
+        self.navigationController = UINavigationController(rootViewController: controller)
+        self.window.rootViewController = self.navigationController
+        self.navigationController.setNavigationBarHidden(true, animated: false)
+        self.window.makeKeyAndVisible()
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+}
+
+extension AppDelegate {
+    
+    func linkNativeCode(controller: FlutterViewController) {
+        setupMethodChannelForSurveyMonkey(controller: controller)
+    }
+    
+    private func setupMethodChannelForSurveyMonkey(controller: FlutterViewController) {
+        
+        let surveyMonkeyChannel = FlutterMethodChannel.init(name: "com.nationwide.thermal_poc/flir", binaryMessenger: controller)
+        
+        surveyMonkeyChannel.setMethodCallHandler { (call, result) in
+            
+            if call.method == "sdkVersion" {
+                let vc = UIStoryboard.init(name: "Main", bundle: .main)
+                        .instantiateViewController(withIdentifier: "CameraViewController") as! SurveyMonkeyViewController
+                if let arguments = call.arguments as? String {
+                    vc.surveyHash = arguments
+                }
+                vc.surveyResult = result
+                self.navigationController.pushViewController(vc, animated: true)
+            }
+        }
+    }
 }
