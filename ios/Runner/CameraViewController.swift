f892734d428676220937620828aeaae4f455674e
Elizabeth Nold <evkearns13@gmail.com>
2020-07-16 12:55:03 -0400

    got it to build w00t

--- ios/Runner/CameraViewController.swift
+++ ios/Runner/CameraViewController.swift
@@ -4,31 +4,31 @@ class CameraViewController: UIViewController {
     
     var surveyResult: FlutterResult!
     var surveyHash: String!
-    private lazy var feedbackController: SMFeedbackViewController? = {
-        let controller = SMFeedbackViewController(survey: surveyHash)
-        controller?.delegate = self
-        controller?.scheduleIntercept(from: self, withAppTitle: "FLIR Integration")
-        return controller
-    }()
+    // private lazy var feedbackController: SMFeedbackViewController? = {
+    //     let controller = SMFeedbackViewController(survey: surveyHash)
+    //     controller?.delegate = self
+    //     controller?.scheduleIntercept(from: self, withAppTitle: "FLIR Integration")
+    //     return controller
+    // }()
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // override func viewDidLoad() {
+    //     super.viewDidLoad()
 
-        feedbackController?.present(from: self, animated: true, completion: nil)
-    }
+    //     feedbackController?.present(from: self, animated: true, completion: nil)
+    // }
 
 }
 
-extension CameraViewController: SMFeedbackDelegate {
-    func respondentDidEndSurvey(_ respondent: SMRespondent!, error: Error!) {
-        if let _ = respondent {
-            surveyResult("Completed")
-            self.navigationController?.popViewController(animated: true)
-        } else if let error = error as? NSError,
-            error.code == 1 {
-            surveyResult("INCOMPLETED")
-            self.navigationController?.popViewController(animated: true)
-        }
-    }
-}
+// extension CameraViewController: SMFeedbackDelegate {
+//     func respondentDidEndSurvey(_ respondent: SMRespondent!, error: Error!) {
+//         if let _ = respondent {
+//             surveyResult("Completed")
+//             self.navigationController?.popViewController(animated: true)
+//         } else if let error = error as? NSError,
+//             error.code == 1 {
+//             surveyResult("INCOMPLETED")
+//             self.navigationController?.popViewController(animated: true)
+//         }
+//     }
+// }
